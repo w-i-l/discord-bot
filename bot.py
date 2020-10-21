65c72359c0d0819ba8052d8d347e7e99b8c7bd28
w-i-l <65015373+w-i-l@users.noreply.github.com>
2020-10-21 23:17:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands,tasks
+import random
+import asyncio
 
 bot=commands.Bot(command_prefix="<k ")
 
@@ -33,6 +35,7 @@ async def hel(ctx):
     embed.add_field(name='Musamalizare cu numar //// sterge',value='Sterge ultimele n mesaje')
     embed.add_field(name='Musamalizare cu nume //// s',value='Sterge ultimul mesaj al prostului')
     embed.add_field(name='Fa o vizita la sclavi //// m',value='Ofera-i prietenului o excursie')
+    embed.add_field(name='Duel in penis //// challange',value='Da pe fata')
     await ctx.send(embed=embed)
 
 
@@ -47,11 +50,18 @@ async def on_command_error(ctx,error):
         await ctx.send("Nu pot asta, ce esti asa prost? "+ctx.message.author.mention)
     elif isinstance(error,commands.CommandOnCooldown):
         await ctx.send(ctx.message.author.mention+' suge-o din nou in: '+str(round(error.retry_after,2))+'s')
+    elif isinstance(error,asyncio.exceptions.TimeoutError):
+        await ctx.send('A expirat timpu cumetre')
 
 
 @bot.command()
 @commands.cooldown(1,300,commands.BucketType.user)
 async def m(ctx,member:discord.Member):
+
+    if ctx.author.voice==None:
+        await ctx.send('Conecteza-te fiti-ar alifia de ras')
+    else:
+        pass
 
     await s(ctx,ctx.message.author)
     channel=list(filter(lambda x: x.name=='sclav',ctx.guild.voice_channels))[0]
@@ -72,12 +82,109 @@ async def m(ctx,member:discord.Member):
         await member.move_to(channel)
         await ctx.send(member.mention+' este abuzat!')
 
+@bot.command()
+async def challange(ctx,member:discord.Member):
+
+    em=discord.Embed(title=ctx.message.author.name,description='Te provoaca'+member.mention,color=discord.Colour.teal())
+    em.add_field(name='Raspunde cu da/nu, y/n',value='daca nu esti pussy')
+
+    await ctx.send(embed=em)
+
+    accept= await bot.wait_for('message',check=lambda m:m.author==member and m.content in ['y','n','da','nu','DA','NU','Da','Nu','Y','N'],timeout=10.0)
+
+    if accept.content.lower() in ['nu','n']:
+        return
+    else:
+        pass
+
+
+    semne=['+','/','-','*']
+
+    numar_1=random.randint(0,101)
+    numar_2=random.randint(0,101)
+    semn=random.choice(semne)
+
+    if semn=='+':
+        rezultat=numar_1+numar_2
+    elif semn=='-':
+        rezultat=numar_1-numar_2
+    elif semn=='/':
+        rezultat=round(numar_1/numar_2,2)
+    elif semn=='*':
+        rezultat=numar_1*numar_2
+
+
+
+    embed=discord.Embed(title='Raspunde primul',description='Care este rezultatul?',color=discord.Colour.blue())
+    embed.add_field(name=str(numar_1)+semn+str(numar_2),value='Maxim 2 zecimale')
+
+    await ctx.send(embed=embed)
+
+    msg=await bot.wait_for('message',check=lambda m:(m.author.id == ctx.message.author.id or m.author.id==member.id) and m.content==str(rezultat),timeout=30.0)
+
+    if msg.author==ctx.message.author:
+
+        sclav=member
+    else:
+
+        sclav=ctx.message.author
+
+    print(rezultat)
+
+    rol_sclav=ctx.guild.get_role(768533926602866709)
+    elita=ctx.guild.get_role(749571862735880202)
+    honor=ctx.guild.get_role(757864291922739280)
+
+    if member.roles[-1].id==749571862735880202:
+        await sclav.remove_roles(elita)
+        await sclav.add_roles(rol_sclav)
+
+        await m(ctx,sclav)
+
+        await asyncio.sleep(5)
+
+
+
+        await sclav.remove_roles(rol_sclav)
+        await sclav.add_roles(elita)
+
+    elif member.roles[-1].id==757864291922739280:
+        await sclav.remove_roles(honor)
+        await sclav.add_roles(rol_sclav)
+
+        await m(ctx,sclav)
+
+        await asyncio.sleep(5)
+
+        await sclav.remove_roles(rol_sclav)
+        await sclav.add_roles(honor)
+    else:
+            await sclav.add_roles(rol_sclav)
+
+            await m(ctx,sclav)
+
+            await asyncio.sleep(5)
+
+            await sclav.remove_roles(rol_sclav)
+
+
+    await ctx.send(sclav.mention+' a fost maltrafoxat')
+
+
 
 @m.after_invoke
 async def reset_cooldown(ctx):
     if ctx.message.author.id==442038246726172683:
         m.reset_cooldown(ctx)
+# 
+# @bot.command()
+# async def rol(ctx,member:discord.Member):
+#     await ctx.send(member.roles[-1].id)
+# @bot.command()
+# async def voice(ctx,member:discord.Member):
+#     if member.voice!=None:
+#         print('connected')
+#     else:
+#         print('not connected')
 
-
-
-bot.run('NzY0ODIwNzk4OTg3ODI5MjQ4.X4L04A.re1tzYNV8AOOEq6ZBEuTr1M4J5k')
+bot.run('NzY0ODIwNzk4OTg3ODI5MjQ4.X4L04A.zuQqEetGhKpom9NiwWbbMuA7UIM')
